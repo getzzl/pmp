2269e3105e6e12a4cf6c1e699e429ed949d5835f
zzl <getzzl@163.com>
2022-11-13 16:28:43 +0800

    删除启动类

--- park-manager-platform-web/src/main/java/com/zk/WebApplication.java
+++ park-manager-platform-web/src/main/java/com/zk/WebApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "com.zk.db.*")
-//@EntityScan(basePackages = "com.zk.common.domain.*")
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
